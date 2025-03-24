f4a52bbb810b2e93baa3f702b79a4f20327772f6
cookie <ambroise.bareux@gmail.com>
2025-03-24 19:10:36 +0100

    separated test

--- src/test.rs
+++ src/test.rs
@@ -3,14 +3,18 @@ mod matrix;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn matrix_creation() {
+    fn matrix_creation_float() {
         use crate::matrix::Matrix;
         
         let custom_data: Vec<Vec<f64>> = vec![vec![0.; 3]; 3];
         let matrix:Matrix = Matrix::from(custom_data);
 
         println!("{:?}", matrix);
-
+    }
+    #[test]
+    fn matrix_creation_int() {
+        use crate::matrix::Matrix;
+        
         let custom_data: Vec<Vec<u64>> = vec![vec![0; 3]; 3];
         let matrix:Matrix = Matrix::from(custom_data);
 
